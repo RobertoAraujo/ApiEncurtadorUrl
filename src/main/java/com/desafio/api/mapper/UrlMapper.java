1924282f487b5b5df5abd289eb2292ecefefeded
Roberto Araujo <endriosrobert@gmail.com>
2022-03-19 10:04:13 -0500

    criaçaõ da classe mapper mapeamento da entidade para o dto

--- src/main/java/com/desafio/api/mapper/UrlMapper.java
+++ src/main/java/com/desafio/api/mapper/UrlMapper.java
@@ -1,2 +1,14 @@
-package com.desafio.api.mapper;public class UrlMapper {
+package com.desafio.api.mapper;
+
+import com.desafio.api.dominio.UrlEntity;
+import com.desafio.api.dto.UrlDTO;
+
+public class UrlMapper {
+    public static UrlEntity toEntity(UrlDTO dot){
+        return merge(new UrlEntity(),dot);
+    }
+    private static UrlEntity merge(UrlEntity url, UrlDTO dto){
+        url.setUrl(dto.getUrl());
+        return url;
+    }
 }
