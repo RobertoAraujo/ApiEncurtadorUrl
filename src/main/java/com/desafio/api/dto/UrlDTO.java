2a79c6d02507eefe050b190bb2ae98ba21536354
Roberto Araujo <endriosrobert@gmail.com>
2022-03-19 10:05:12 -0500

    Criação da classe urlDto

--- src/main/java/com/desafio/api/dto/UrlDTO.java
+++ src/main/java/com/desafio/api/dto/UrlDTO.java
@@ -1,4 +1,34 @@
 package com.desafio.api.dto;
 
-public class UrlDTO {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UrlDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String url;
+
+    public UrlDTO() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UrlDTO urlDTO = (UrlDTO) o;
+        return Objects.equals(url, urlDTO.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url);
+    }
 }
