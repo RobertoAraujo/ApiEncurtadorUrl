d279dd3f6d139b5a34117ed8236ce07eb22de4e7
Roberto Araujo <endriosrobert@gmail.com>
2022-03-19 09:59:43 -0500

    Configuração da repository

--- src/main/java/com/desafio/api/repository/UrlRepository.java
+++ src/main/java/com/desafio/api/repository/UrlRepository.java
@@ -1,9 +1,9 @@
 package com.desafio.api.repository;
 
-import com.desafio.api.dominio.Url;
+import com.desafio.api.dominio.UrlEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApiRepository extends JpaRepository<Url, Long> {
+public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
 }
