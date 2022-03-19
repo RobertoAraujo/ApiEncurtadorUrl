ce2a2e54e2ffa5266c4a66ec98cf1de3117157b4
Roberto Araujo <endriosrobert@gmail.com>
2022-03-19 08:55:37 -0500

    criação da classe url

--- src/main/java/com/desafio/api/dominio/Url.java
+++ src/main/java/com/desafio/api/dominio/Url.java
@@ -1,7 +1,72 @@
 package com.desafio.api.dominio;
 
-public class Url {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Url implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
     private Long id;
     private String url;
+    private LocalDateTime dataEntrada;
+    private LocalDateTime dataUpdate;
 
+    public Url() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public LocalDateTime getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(LocalDateTime dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public LocalDateTime getDataUpdate() {
+        return dataUpdate;
+    }
+
+    public void setDataUpdate(LocalDateTime dataUpdate) {
+        this.dataUpdate = dataUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Url url1 = (Url) o;
+        return Objects.equals(id, url1.id) && Objects.equals(url, url1.url) && Objects.equals(dataEntrada, url1.dataEntrada) && Objects.equals(dataUpdate, url1.dataUpdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, url, dataEntrada, dataUpdate);
+    }
+
+    @Override
+    public String toString() {
+        return "Url{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                ", dataEntrada=" + dataEntrada +
+                ", dataUpdate=" + dataUpdate +
+                '}';
+    }
 }
