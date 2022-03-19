558e2ad34818a340e1b18b838822ed266d8b1608
Roberto Araujo <endriosrobert@gmail.com>
2022-03-19 09:58:19 -0500

    criaçaõ dos methodos salvar e converte url

--- src/main/java/com/desafio/api/service/UrlService.java
+++ src/main/java/com/desafio/api/service/UrlService.java
@@ -1,4 +1,23 @@
 package com.desafio.api.service;
 
+import com.desafio.api.dominio.UrlEntity;
+import com.desafio.api.dto.UrlDTO;
+import com.desafio.api.repository.UrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UrlService {
+
+    @Autowired
+    private UrlRepository repository;
+
+    public void salva(UrlEntity url){
+        repository.save(url);
+    }
+    public UrlEntity converter(UrlEntity url){
+        String antigaUrl = url.getUrl();
+        String novaUrl = antigaUrl.replace("http://www.", "");
+        return url;
+    }
 }
