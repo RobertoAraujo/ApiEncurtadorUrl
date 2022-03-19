3cfdbaf71f5264fe12b9cdcb28b37e1e2eb5e77c
Roberto Araujo <endriosrobert@gmail.com>
2022-03-19 09:56:27 -0500

    criação do endpoint para salvar a url no banco

--- src/main/java/com/desafio/api/contoller/ApiController.java
+++ src/main/java/com/desafio/api/contoller/ApiController.java
@@ -1,4 +1,24 @@
 package com.desafio.api.contoller;
 
+import com.desafio.api.dominio.UrlEntity;
+import com.desafio.api.dto.UrlDTO;
+import com.desafio.api.mapper.UrlMapper;
+import com.desafio.api.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/url")
 public class ApiController {
+    @Autowired
+    private UrlService service;
+
+    @PostMapping(value = "/")
+    public ResponseEntity<UrlDTO> salvarUrl(@RequestBody UrlDTO urlDto){
+        UrlEntity urlEntity = UrlMapper.toEntity(urlDto);
+        service.salva(urlEntity);
+        UrlEntity converter = service.converter(urlEntity);
+        return ResponseEntity.ok().body(urlDto);
+    }
 }
