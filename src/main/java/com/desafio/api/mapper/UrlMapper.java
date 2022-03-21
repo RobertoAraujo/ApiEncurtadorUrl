230a4935f2ba1296a686706a725e2a55a9e6626c
Roberto Araujo <endriosrobert@gmail.com>
2022-03-21 08:45:13 -0500

    Criação da urlMapper

--- src/main/java/com/desafio/api/mapper/UrlMapper.java
+++ src/main/java/com/desafio/api/mapper/UrlMapper.java
@@ -2,13 +2,24 @@ package com.desafio.api.mapper;
 
 import com.desafio.api.dominio.UrlEntity;
 import com.desafio.api.dto.UrlDTO;
+import com.desafio.api.util.UrlUtil;
 
 public class UrlMapper {
     public static UrlEntity toEntity(UrlDTO dot){
         return merge(new UrlEntity(),dot);
     }
     private static UrlEntity merge(UrlEntity url, UrlDTO dto){
+        url.setUrlEncurtada(UrlUtil.buildAlias(dto.getUrl()));
         url.setUrl(dto.getUrl());
         return url;
     }
+
+    public static UrlDTO toDTO(UrlEntity entity){
+        return merge(new UrlDTO(),entity);
+    }
+    private static UrlDTO merge(UrlDTO dto, UrlEntity url){
+        dto.setUrl(url.getUrl());
+        return dto;
+    }
+
 }
