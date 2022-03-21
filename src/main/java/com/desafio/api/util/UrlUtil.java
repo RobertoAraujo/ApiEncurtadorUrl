4f6f86cbd335765006d8dde146d574e42e315fc7
Roberto Araujo <endriosrobert@gmail.com>
2022-03-21 08:42:03 -0500

    criação da classe url util para a remoçao dos protocolo via replace

--- src/main/java/com/desafio/api/util/UrlUtil.java
+++ src/main/java/com/desafio/api/util/UrlUtil.java
@@ -1,2 +1,14 @@
-package com.desafio.api.util;public class UrlUtil {
+package com.desafio.api.util;
+
+public class UrlUtil {
+    public static String buildAlias(String url) {
+        String novaUrlEncurtada = url;
+        novaUrlEncurtada = novaUrlEncurtada.replace("https://www.", "");
+        novaUrlEncurtada = novaUrlEncurtada.replace("http://www.", "");
+        novaUrlEncurtada = novaUrlEncurtada.replace("https://", "");
+        novaUrlEncurtada = novaUrlEncurtada.replace("http://", "");
+
+        novaUrlEncurtada = novaUrlEncurtada.substring(0, novaUrlEncurtada.indexOf("/"));
+        return novaUrlEncurtada;
+    }
 }
