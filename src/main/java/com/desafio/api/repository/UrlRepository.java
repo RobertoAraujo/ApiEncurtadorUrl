9aa48069600da8de6882f2a951d8e18aa5882a69
Roberto Araujo <endriosrobert@gmail.com>
2022-03-21 08:48:05 -0500

    Pequeno problema na classe repository mudei para crudRepository deu o mesmo problema então retornei para JpaRepository

--- src/main/java/com/desafio/api/repository/UrlRepository.java
+++ src/main/java/com/desafio/api/repository/UrlRepository.java
@@ -4,6 +4,9 @@ import com.desafio.api.dominio.UrlEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+    Optional<UrlEntity> findByUrlEncurtada(String urlEncurtada);
 }
